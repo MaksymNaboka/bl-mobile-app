aee75e512740b491606b238366975dd795817cf2
Slimymaks <maksnaboka1@gmail.com>
2024-06-19 14:06:24 -0400

    testing husky

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -9,17 +9,11 @@ import { ThemedView } from "@/components/ThemedView";
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+    <ParallaxScrollView headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      <ThemedView style={styles.titleContainer}><ThemedText type="title">Explore</ThemedText>
       </ThemedView>
       <ThemedText>
-        This app includes example code to help you get started.!!!
+        This app includes example code to help you get started.!
       </ThemedText>
       <Collapsible title="File-based routing!">
         {" "}
