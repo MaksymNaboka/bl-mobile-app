335dba0de26e2403a8bd25a2710553f18b2cd7d8
Slimymaks <maksnaboka1@gmail.com>
2024-06-19 14:39:30 -0400

    test

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -13,7 +13,7 @@ export default function TabTwoScreen() {
       <ThemedView style={styles.titleContainer}><ThemedText type="title">Explore</ThemedText>
       </ThemedView>
       <ThemedText>
-        This app includes example code to help you get started.!
+        This app includes example code to help you get started.
       </ThemedText>
       <Collapsible title="File-based routing!">
         {" "}
