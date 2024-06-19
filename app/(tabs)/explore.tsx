8007853223ca7da41761fe1d2fac3c7417924498
Slimymaks <maksnaboka1@gmail.com>
2024-06-19 17:03:19 -0400

    test

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -10,15 +10,14 @@ import React from "react";
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+    <ParallaxScrollView headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
       headerImage={
         <Ionicons size={310} name="code-slash" style={styles.headerImage} />
       }
     >
       <ThemedView style={styles.titleContainer}>
         {" "}
-        <ThemedText type="title">Explore!</ThemedText>
+        <ThemedText type="title">Explore</ThemedText>
       </ThemedView>{" "}
       <ThemedText>
         This app includes example code to help you get started.
