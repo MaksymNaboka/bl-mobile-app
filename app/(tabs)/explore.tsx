0d8d28feb3aed42610a6c2afe35df985ca34ac31
Slimymaks <maksnaboka1@gmail.com>
2024-06-20 12:57:26 -0400

    Update explore.tsx

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -10,8 +10,7 @@ import { ThemedView } from "@/components/ThemedView";
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+    <ParallaxScrollView headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
       headerImage={
         <Ionicons size={310} name="code-slash" style={styles.headerImage} />
       }
